6fbfd45201230677fae5f205c5d70e3806f55401
It24103917 <it24103917@my.sliit.lk>
2025-10-01 23:45:56 +0530

    IT24103917

---
+++ Lab Sheet 09/IT24103917.R
@@ -0,0 +1,11 @@
+setwd("C:\\Users\\Asus\\Desktop\\IT24103917")
+
+#Part_01
+
+set.seed(123)   
+sample_data <- rnorm(25, mean = 45, sd = 2)
+sample_data
+
+#Part_02
+
+t.test(sample_data, mu = 46, alternative = "less", conf.level = 0.95)
